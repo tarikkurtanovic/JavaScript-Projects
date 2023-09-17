58eb4ec6596fb6a7ec39b6b07b77b37b4d79b374
tarikkurtanovic <tarik2371@gmail.com>
2023-09-17 18:10:59 +0200

    Age calculator finished

--- Age calculator/script.js
+++ Age calculator/script.js
@@ -6,7 +6,7 @@ userInput.max = new Date().toISOString().split("T")[0];
 function calculateAge() {
   let birthDate = new Date(userInput.value);
   let y1 = birthDate.getFullYear();
-  let m1 = birthDate.getMonth();
+  let m1 = birthDate.getMonth() + 1;
   let d1 = birthDate.getDate();
 
   console.log(m1, d1, y1);
@@ -15,12 +15,35 @@ function calculateAge() {
   console.log(today);
 
   let y2 = today.getFullYear();
-  let m2 = today.getMonth();
+  let m2 = today.getMonth() + 1;
   let d2 = today.getDate();
 
-  result.innerHTML = `You are ${y2 - y1} years, ${m2 - m1} months and ${
-    d2 - d1
-  } days old`;
+  let y3, m3, d3;
+
+  y3 = y2 - y1;
+  if (m2 >= m1) {
+    m3 = m2 - m1;
+  } else {
+    y3--;
+    m3 = 12 + m2 - m1;
+  }
+
+  if (d2 >= d1) {
+    d3 = d2 - d1;
+  } else {
+    m3--;
+    d3 = getDaysInMonth(y1, m1) + d2 - d1;
+  }
+
+  if (m3 < 0) {
+    m3 = 11;
+    y3--;
+  }
+  result.innerHTML = `You are <span>${y3}</span> years,  <span>${m3}</span> months and <span>${d3}</span> days old`;
 
   result.style.display = "block";
+}
+
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
 }
